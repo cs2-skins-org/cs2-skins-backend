44b694ecd44f2f541b754eaf0bde385cc302a503
rosmontis0328 <rosmontis@gmail.com>
2025-05-25 21:18:49 +0200

    f

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,8 +6,6 @@ import { UseGuards, Request } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { Request as ExpressRequest } from 'express';
 
-
-
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
