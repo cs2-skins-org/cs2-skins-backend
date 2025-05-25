3c2f33ca53fb3049b6a2627e58fcbe2118e8730f
Arthur Jakypbek <arthurjkk43@gmail.com>
2025-05-25 13:21:31 +0200

    added balance to users

--- src/sticker/dto/create-sticker.dto.ts
+++ src/sticker/dto/create-sticker.dto.ts
@@ -1,7 +1,13 @@
-import { StickerRarity } from "../entities/stickerrarity.enum";
+import { IsString } from 'class-validator';
+import { StickerRarity } from '../entities/stickerrarity.enum';
 
 export class CreateStickerDto {
+  @IsString()
   name: string;
+
+  @IsString()
   rarity: StickerRarity;
+
+  @IsString()
   image_path: string;
 }
